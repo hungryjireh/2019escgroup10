b8f38a3c46cf74e121fb627b5dd2272233959769
Jireh <jireh_tan@mymail.sutd.edu.sg>
2019-03-14 22:07:37 +0800

    edited ticket_system settings.py to hide secretkey

--- ticket_system/ticket_system/settings.py
+++ ticket_system/ticket_system/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9w*e2)5*y26%=(s3x+=#ad_dkl&3ol3+e-ki@+^iq)*tb)#hn3'
+SECRET_KEY = '********'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
