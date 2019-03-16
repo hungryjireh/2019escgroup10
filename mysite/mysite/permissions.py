0d865b7676669da819e703e60f62bb12104eda79
Jireh <jireh_tan@mymail.sutd.edu.sg>
2019-03-16 15:37:41 +0800

    updated permissions for views, wrote tests

---
+++ mysite/mysite/permissions.py
@@ -0,0 +1,6 @@
+from rest_framework import permissions
+
+class IsSuperUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        return user.is_authenticated and user.is_superuser
